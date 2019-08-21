86dcfc06f146f6d936d00dee6c72e8564e04a96a
uwejanssen19 <uwe@janssen-muenchen.de>
2019-08-21 15:51:16 +0200

    changed fil timer to 240 an added comments to rules#

--- irrigation_bash_scripts/irrLib.sh
+++ irrigation_bash_scripts/irrLib.sh
@@ -85,8 +85,8 @@ function isRaining() {
 }
 
 function getForeCast() {
-  # retrieve 3*8 hours' forecast , i.e. 24h 
-  wget "http://api.openweathermap.org/data/2.5/forecast?id=2886446&units=metric&lang=de&cnt=8&appid=$OW_API" -O $FORECAST
+  # retrieve 4*3 hours' forecast , i.e. 12h 
+  wget "http://api.openweathermap.org/data/2.5/forecast?id=2886446&units=metric&lang=de&cnt=4&appid=$OW_API" -O $FORECAST
 }
 function willRain() {
   getForeCast
