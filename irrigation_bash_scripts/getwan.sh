5ab547a10389253323e2a74080920e9f0e085ab5
uwejanssen19 <uwe@janssen-muenchen.de>
2019-10-18 17:07:43 +0200

    working revision

--- irrigation_bash_scripts/getwan.sh
+++ irrigation_bash_scripts/getwan.sh
@@ -1,1 +1,6 @@
-wget -qO - http://ipecho.net/plain | xargs echo
+#!/bin/bash
+source /home/pi/scripts/irrLib.sh
+log "$0 ENTRY"
+number=$(wget -qO - http://ipecho.net/plain | xargs echo)
+log "$0 EXIT, result = $number"
+echo $number
