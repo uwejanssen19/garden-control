12ba539b78c4f4caec87960984d184a1ab38ebfa
uwejanssen19 <uwe@janssen-muenchen.de>
2019-10-18 17:09:54 +0200

    working revision

--- irrigation_bash_scripts/getRandom.sh
+++ irrigation_bash_scripts/getRandom.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 RANGE=30
 FLOOR=2
+source /home/pi/scripts/irrLib.sh
+log "$0 ENTRY"
 # May combine above two techniques to retrieve random number between two limits.
 number=0   #initialize
 while [ "$number" -le $FLOOR ]
@@ -8,5 +10,5 @@ do
   number=$RANDOM
   let "number %= $RANGE"  # Scales $number down within $RANGE.
 done
+log "$0 EXIT, result = $number"
 echo "Random number between $FLOOR and $RANGE ---  $number"
-echo
