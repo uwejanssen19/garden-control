c1161fd3e6d0a2d580e5405d113f1c947130cfb6
uwejanssen19 <uwe@janssen-muenchen.de>
2020-11-06 20:41:52 +0100

    cleanup and rearrange folders

--- esp8266_modules/build/core/libb64/cencode.cpp.d
+++
@@ -1,3 +0,0 @@
-p:\dokumente\uwe\software_projekte\rasperry_pi\My_Raspberry_Pis\garden-control\esp8266_modules\build\core\libb64\cencode.cpp.o: \
- C:\Users\uwe\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.7.4\cores\esp8266\libb64\cencode.cpp \
- C:\Users\uwe\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.7.4\cores\esp8266\libb64\cencode.h
--- esp8266_modules/build/libraries/U8g2/clib/u8log_u8x8.c.d
+++
@@ -1,9 +0,0 @@
-p:\dokumente\uwe\software_projekte\rasperry_pi\My_Raspberry_Pis\garden-control\esp8266_modules\build\libraries\U8g2\clib\u8log_u8x8.c.o: \
- C:\Users\uwe\Documents\Arduino\libraries\U8g2\src\clib\u8log_u8x8.c \
- C:\Users\uwe\Documents\Arduino\libraries\U8g2\src\clib\u8x8.h \
- C:\Users\uwe\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.7.4/tools/sdk/libc/xtensa-lx106-elf/include/stdint.h \
- C:\Users\uwe\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.7.4/tools/sdk/libc/xtensa-lx106-elf/include/machine/_default_types.h \
- C:\Users\uwe\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.7.4/tools/sdk/libc/xtensa-lx106-elf/include/sys/features.h \
- C:\Users\uwe\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.7.4/tools/sdk/libc/xtensa-lx106-elf/include/sys/_intsup.h \
- C:\Users\uwe\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.7.4/tools/sdk/libc/xtensa-lx106-elf/include/limits.h \
- C:\Users\uwe\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.7.4/tools/sdk/libc/xtensa-lx106-elf/include/newlib.h
