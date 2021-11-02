ac7b77827e98038839d7c06e186bfb80233759b4
uwejanssen19 <uwe@janssen-muenchen.de>
2021-11-02 21:56:41 +0100

    added jsondb

--- openhab/automation/jsr223/astro.js
+++ openhab/automation/jsr223/astro.js
@@ -74,53 +74,37 @@
             var mqttActions = actions.get("mqtt","mqtt:broker:myUnsecureBroker");
             mqttActions.publishMQTT("astroevent", convertedEvent, true);
 
-        function makeAstroEventHumanReadable (pEventString) {
+            function makeAstroEventHumanReadable (pEventString) {   
+            // Format of event :    
+            //   astro:sun:local:morningNight#event triggered START   
+            //   [0]  :[1]:[2] :[3]   
+            // we want to return the "morningNight" part   
+            //log.info("makeAstroEventHumanReadable::pEventString = " + pEventString + " type  = " + typeof pEventString);   
+            var astroEventName = pEventString.toString();   
+            //log.info("makeAstroEventHumanReadable::astrooEventName   = " + astroEventName);   
+            astroParts = astroEventName.split(":");   
+            // find last chunk separated by ':'   
+            lastPart = astroParts[astroParts.length-1];   
+            //log.info("makeAstroEventHumanReadable::lastPart  = "+ lastPart);   
+            // now we got "<eventName>#event triggered START|END"   
+            // split again to get the actual event   
+            astroParts2 = lastPart.split("#");   
+            //log.info("makeAstroEventHumanReadable::astroParts2 = {}", astroParts2);  
+            // map file contains:  
+            // sunrise | sunset for the sun BUT  
+            // rise | set for the moon   
+            // (not very tidy but i am too lazy since I am not even sure wether to keep moon rise and set anyway)
 
-        // Format of event : 
-
-        //   astro:sun:local:morningNight#event triggered START
-
-        //   [0]  :[1]:[2] :[3]
-
-        // we want to return the "mornigNight" part
-
-        //log.info("makeAstroEventHumanReadable::pEventString = " + pEventString + " type  = " + typeof pEventString);
-
-        var astroEventName = pEventString.toString();
-
-        //  log.info("makeAstroEventHumanReadable::astroEventName   = " + astroEventName);
-
-        astroParts = astroEventName.split(":");
-
-        // find last chunk separated by ':'
-
-        lastPart = astroParts[astroParts.length-1];
-
-        //  log.info("makeAstroEventHumanReadable::lastPart  = "+ lastPart);
-
-        // now we got "<eventName>#event triggered START|END"
-
-        // split again to get the actual event
-
-        astroParts2 = lastPart.split("#");
-
-        //  log.info("makeAstroEventHumanReadable::astroParts2 = {}", astroParts2);
-          if ((astroParts[1] === "sun") && (astroParts2[1] === "rise" || astroParts2[1] === "set")) {
-            eventName = astroParts[1]+astroParts2[0];
-          } else {
-            eventName = astroParts2[0];
-          }
-        //  log.info("makeAstroEventHumanReadable::eventName = {}", eventName);
-
-        astroMapFile = "astroevents.map";
-
-        var Transformation = Java.type("org.openhab.core.transform.actions.Transformation");
-
-        userFriendlyName = Transformation.transform("MAP",astroMapFile , eventName);
-
-        log.info("makeAstroEventHumanReadable::userFriendlyName = {}", userFriendlyName);
-
-        return userFriendlyName;
-
-        }
-    type: script.ScriptAction
+              if ((astroParts[1] === "sun") && (astroParts2[0] === "rise" || astroParts2[0] === "set")) {
+                eventName = astroParts[1]+astroParts2[0];
+              } else {
+                eventName = astroParts2[0];
+              }
+                log.info("makeAstroEventHumanReadable::eventName = {}", eventName);   
+              astroMapFile = "astroevents.map";   
+              var Transformation = Java.type("org.openhab.core.transform.actions.Transformation");   
+              userFriendlyName = Transformation.transform("MAP",astroMapFile , eventName);   
+              //log.info("makeAstroEventHumanReadable::userFriendlyName = {}", userFriendlyName);   
+              return userFriendlyName; 
+            }
+    // type: script.ScriptAction
