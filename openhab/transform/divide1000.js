1dc8cd9e0ded62f122339123990b3e71825aa12b
uwejanssen19 <uwe@janssen-muenchen.de>
2022-11-30 16:34:12 +0100

    added modbus access to HUAWEI

---
+++ openhab/transform/divide1000.js
@@ -0,0 +1,8 @@
+// Wrap everything in a function (no global variable pollution)
+// variable "input" contains data passed by openHAB
+(function(inputData) {
+    // on read: the polled number as string
+    // on write: openHAB command as string
+    var DIVIDE_BY = 1000;
+    return parseFloat(inputData) / DIVIDE_BY;
+})(input)
