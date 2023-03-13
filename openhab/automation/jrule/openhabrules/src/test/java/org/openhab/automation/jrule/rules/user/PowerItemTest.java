45488d6f9d785123bb41598546efbd74007a7f38
uwejanssen19 <uwe@janssen-muenchen.de>
2023-03-13 19:30:15 +0100

    fixed tests

--- openhab/automation/jrule/openhabrules/src/test/java/org/openhab/automation/jrule/rules/user/PowerItemTest.java
+++ openhab/automation/jrule/openhabrules/src/test/java/org/openhab/automation/jrule/rules/user/PowerItemTest.java
@@ -1,6 +1,9 @@
 package org.openhab.automation.jrule.rules.user;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.Test;
 
 public class PowerItemTest {
@@ -32,5 +35,14 @@ public class PowerItemTest {
     @Test
     public void checkDeviceInMapBureauIR() {
         assertTrue(PowerItems.getInstance().isDeviceInMap(PowerItems.HEATING_DEVICE.Bureau_IR_work_state));
+    }
+    @Test
+    public void isInRangeForValidValue() {
+        PowerItems.HEATING_STATES state = PowerItems.HEATING_STATES.valueOf("standby");
+        assertEquals(PowerItems.HEATING_STATES.standby,state);
+    }
+    @Test
+    public void isInRangeForInValidValue() {
+        assertThrows(IllegalArgumentException.class,() -> {PowerItems.HEATING_STATES.valueOf("unknown state");});
     }
 }
